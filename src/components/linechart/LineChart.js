a3e790f272f23185e0f2aaaca8943db478ad2ac5
mehdi <mehdi.allahyari@gmail.com>
2023-01-22 10:14:17 -0500

    tooltip code added to the linechart.

--- src/components/linechart/LineChart.js
+++ src/components/linechart/LineChart.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as d3 from 'd3';
 import { Box } from '@mui/material';
 import AxisBottom from './AxisBottom';
 import AxisLeft from './AxisLeft';
+
+import './styles.css';
 
 const dimensions = {
   width: 800,
@@ -16,6 +18,11 @@ export const LineChart = ({ data }) => {
     width: dimensions.width - margin.left - margin.right,
     height: dimensions.height - margin.top - margin.bottom,
   };
+
+  const [tooltipData, setTooltipData] = useState({});
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [xPos, setXPos] = useState(0);
+  const [yPos, setYPos] = useState(0);
 
   // Define accessors
   const dateParser = d3.timeParse('%Y-%m-%d');
@@ -44,8 +51,29 @@ export const LineChart = ({ data }) => {
     .x((d) => xScale(xAccessor(d)))
     .y((d) => yScale(yAccessor(d)));
 
+  const handleMouseMove = (e) => {
+    const mouseXYPos = d3.pointer(e);
+    const xPos = mouseXYPos[0];
+    const hoveredDate = xScale.invert(xPos);
+    const bisectDate = d3.bisector((d) => xAccessor(d)).right;
+    const closestIndex = bisectDate(data, hoveredDate);
+    if (closestIndex >= data.length) return;
+    const closestDataPoint = data[closestIndex];
+    const closestXValue = xAccessor(closestDataPoint);
+    const closestYValue = yAccessor(closestDataPoint);
+    const newTooltipData = {
+      data: closestDataPoint,
+      x: xScale(closestXValue) + dimensions.margin.left,
+      y: yScale(closestYValue),
+    };
+    setTooltipData(newTooltipData);
+    setShowTooltip(true);
+    setXPos(() => xScale(closestXValue));
+    setYPos(() => yScale(closestYValue));
+  };
+
   return (
-    <Box id="wrapper">
+    <div id="wrapper">
       <svg width={width} height={height} style={{ backgroundColor: '#F5F3F2' }}>
         <g transform={`translate(${margin.left},${margin.top})`}>
           <path
@@ -88,8 +116,61 @@ export const LineChart = ({ data }) => {
           >
             {yAxisLabel}
           </text>
+          <rect
+            width={boundedDimensions.width}
+            height={boundedDimensions.height}
+            fill="transparent"
+            onMouseMove={(e) => handleMouseMove(e)}
+            onMouseLeave={() => setShowTooltip(false)}
+          />
+          {showTooltip && (
+            <>
+              <circle
+                cx={xPos}
+                cy={yPos}
+                r="3"
+                fill="white"
+                stroke="#BD2D28"
+                strokeWidth="2"
+                pointerEvents="none"
+              />
+              <line
+                x1={xPos}
+                y1={0}
+                x2={xPos}
+                y2={boundedDimensions.height}
+                stroke="#0F8C79"
+                strokeWidth="2"
+                strokeDasharray="5,4"
+                pointerEvents="none"
+              />
+            </>
+          )}
         </g>
       </svg>
-    </Box>
+      {showTooltip && <Tooltip tooltipData={tooltipData} />}
+    </div>
+  );
+};
+
+const Tooltip = ({ tooltipData }) => {
+  const { data } = tooltipData;
+  return (
+    <div
+      className="tooltip"
+      style={{
+        transform: `translate(calc(-50% + ${tooltipData.x}px), calc(-100% + ${tooltipData.y}px))`,
+      }}
+    >
+      <div className="tooltip-info">
+        <p>
+          <strong>Date: </strong>
+          {data.Date}
+        </p>
+        <p>
+          <strong>Price: </strong> {Number.parseFloat(data.Close).toFixed(2)}
+        </p>
+      </div>
+    </div>
   );
 };
